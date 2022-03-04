502dbc8b4130b75d9a08a791f06e212c2fa7f5cc
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-03-04 22:02:46 +0300

    Exercise2_3 Completed

--- Exercise2_3/Program.cs
+++ Exercise2_3/Program.cs
@@ -10,6 +10,22 @@ namespace Exercise2_3
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите четное или нечетное число:");
+
+
+            byte num = byte.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Четное число");
+            }
+
+
+            else
+            {
+                Console.WriteLine("Нечетное число");
+            }
+
         }
     }
 }
